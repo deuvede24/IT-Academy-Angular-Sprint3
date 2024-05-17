e09a65883689d17939f82344b5bd8c1519e19011
deuvede24 <cdaniela.vd@gmail.com>
2024-05-17 13:06:15 +0200

    all ex. done&tested

--- src/films.js
+++ src/films.js
@@ -3,10 +3,6 @@ function getAllDirectors(array) {
   let result = array.map((movie) => movie.director);
   console.log('EXERCICE 1 ->', result);
   return result;
-
-  /*let result =  ???;
-  console.log("EXERCICE 1 ->", result);
-  return result;*/
 }
 
 // Exercise 2: Get the films of a certain director
@@ -18,10 +14,6 @@ function getMoviesFromDirector(array, director) {
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-  /* let moviesFromDirector = array.filter((movie) => movie.director === director);
-   let totalScore = moviesFromDirector.reduce((total, movie) => total + movie.score, 0);
-   let result = (totalScore / moviesFromDirector.length).toFixed(2);
-   return parseFloat(result);*/
   let moviesFromDirector = getMoviesFromDirector(array, director);
   let totalScore = Number(
     moviesFromDirector.reduce((total, movie) => total + movie.score, 0)
@@ -35,7 +27,7 @@ function moviesAverageOfDirector(array, director) {
 function orderAlphabetically(array) {
   const sortedMovies = array
     .map((movie) => movie.title)
-    .sort((a, b) => a.localeCompare(b)) //LocalCompare garantiza orden alfabéitco sensible a mayúsuclas-minúsculas y caracteres especiales.
+    .sort((a, b) => a.localeCompare(b)) //localCompare garantiza orden alfabéitco sensible a mayúsuclas-minúsculas y caracteres especiales.
     .slice(0, 20); //las primeras 20
   console.log('EXERCICE 4 ->', sortedMovies);
   return sortedMovies;
@@ -43,32 +35,28 @@ function orderAlphabetically(array) {
 
 // Exercise 5: Order by year, ascending
 function orderByYear(array) {
-  // Utilizamos sort() para ordenar las películas por año y luwgo por tittle
+  // Utilizamos sort() para ordenar las películas por año y luego por tittle
   const copyArray = [...array];
   const sortedYearTitle = copyArray.sort((a, b) => {
     if (a.year !== b.year) {
       // Comparamos los años
-      return a.year - b.year; // Orden ascendente por año
+      return a.year - b.year; //Orden ascendente por año
     } else {
       // Si las películas tienen el mismo año, las ordenamos por tittle
       return a.title.localeCompare(b.title);
     }
   });
-
-  return sortedYearTitle; // Devolvemos el array de películas ordenadas
+  return sortedYearTitle; //Devolvemos el array de películas ordenadas
 }
 
 // Exercise 6: Calculate the average of the movies in a category
-
 function moviesAverageByCategory(array, genre) {
   // Filtrar movies por genre
   const genreMovies = array.filter((movie) => movie.genre.includes(genre));
-
   // Si no hay películas del género, retornar 0
   if (genreMovies.length === 0) {
     return 0;
   }
-
   // Calcular total score de este genre
   const totalScore = parseFloat(
     genreMovies.reduce((total, movie) => total + movie.score, 0)
@@ -81,29 +69,6 @@ function moviesAverageByCategory(array, genre) {
 
 // Exercise 7: Modify the duration of movies to minutes
 function hoursToMinutes(array) {
-  /* const modifyDuration = array.map(movie => {
-     //Extraer horas y minutos utilizando el método match y regEx
-     const match = movie.duration.match(/(\d+)h (\d+)min/);
- 
-     if (match) {
-       // Calcular la duración en minutos sumando las horas convertidas a minutos y los minutos
-       let hoursToMinutes = Number(match[1]) * 60;
-       let minutes = Number(match[2]);
-       let totalMinutes = hoursToMinutes + minutes;
- 
-       // Devolver copia del array cambiando la duration por la modificada en minutes
-       return {
-         ...movie,
-         duration: totalMinutes
-       };
-     } else {
-       // Si el formato no coincide, devolver la película sin modificar la duración
-       return movie;
-     }
-   });
- 
-   return modifyDuration;*/
-
   const modifyDuration = array.map((movie) => {
     //Copia del array
     let totalMinutes = 0;
@@ -132,13 +97,11 @@ function hoursToMinutes(array) {
 function bestFilmOfYear(array, year) {
   const sameYearBestScore = array
     .filter((movie) => movie.year === year)
-    .reduce((a, b) => (a.score > b.score ? a : b));
-  //  const bestScore = parseFloat(sameYear.sort((a, b) => ));
+    .reduce((a, b) => (a.score > b.score ? a : b));//comparo los score y siempre me quedo con el mayor
 
-  return [sameYearBestScore]; // retornar array
+  return [sameYearBestScore]; //[] para retornar array
 }
 
-//una funció que accepti l'any, i retorni la millor pel·lícula d'aquest any.
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
